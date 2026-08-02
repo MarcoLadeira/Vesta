587b8a2334b37590ebb045a38beb4b594aec9b7c
Marco <fristtysteam@gmail.com>
2026-08-02 19:48:04 +0100

    test: add PR capability demonstration test

---
+++ tests/test_pr_capability.py
@@ -0,0 +1,6 @@
+"""Test to demonstrate PR creation capability."""
+
+
+def test_pr_capability_works():
+    """Verify that PR creation works end-to-end."""
+    assert True, "PR capability test passed"
